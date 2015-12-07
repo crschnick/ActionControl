30aa64689ae2affdbc1cb769472e5b7bdbe2330e
Christopher Schnick <c.schnick@monospark.org>
2015-12-07 11:51:11 +0100

    Initial commit

---
+++ src/main/java/org/monospark/actionpermissions/ActionPermissions.java
@@ -0,0 +1,8 @@
+package org.monospark.actionpermissions;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "actionpermissions", name = "ActionPermissions", version = "1.0")
+public class ActionPermissions {
+
+}
