22c910e4c9f628ca915cadc21477f9dce577ec11
Christopher Schnick <c.schnick@monospark.org>
2015-12-12 23:54:07 +0100

    Add action support for block placing

---
+++ src/main/java/org/monospark/actionpermissions/place/BlockPlaceHandler.java
@@ -0,0 +1,45 @@
+package org.monospark.actionpermissions.place;
+
+import java.util.Optional;
+
+import org.monospark.actionpermissions.ActionHandler;
+import org.monospark.actionpermissions.group.Group;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.block.ChangeBlockEvent.Place;
+
+import com.google.common.collect.Sets;
+import com.google.gson.JsonDeserializer;
+
+public class BlockPlaceHandler extends ActionHandler<ChangeBlockEvent.Place, BlockPlaceSettings> {
+
+	public BlockPlaceHandler() {
+		super("placeBlock", BlockPlaceSettings.class, ChangeBlockEvent.Place.class);
+	}
+
+	@Override
+	public void handle(Place event) throws Exception {
+		Optional<Player> player = event.getCause().first(Player.class);
+		if(player.isPresent()) {
+			Optional<Group> playerGroup = Group.getRegistry().getGroup(player.get());
+			if(playerGroup.isPresent()) {
+				Group group = playerGroup.get();
+				BlockPlaceSettings settings = group.getActionSettings(this);
+				boolean allowed = settings.canPlace(event.getTransactions().get(0).getFinal().getState());
+				if(!allowed) {
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+
+	@Override
+	public BlockPlaceSettings uniteSettings(BlockPlaceSettings s1, BlockPlaceSettings s2) {
+		return new BlockPlaceSettings(s1.getResponse(), Sets.union(s1.getMatchers(), s2.getMatchers()));
+	}
+	
+	@Override
+	public JsonDeserializer<BlockPlaceSettings> getSettingsDeserializer() {
+		return new BlockPlaceSettings.Deserializer();
+	}
+}
