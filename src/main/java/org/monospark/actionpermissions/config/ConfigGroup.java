1d42616b75ac7f9dee6cafe4f86fbace1e1e8f25
Christopher Schnick <c.schnick@monospark.org>
2015-12-12 23:52:45 +0100

    Add support for loading groups from a config file

---
+++ src/main/java/org/monospark/actionpermissions/config/ConfigGroup.java
@@ -0,0 +1,56 @@
+package org.monospark.actionpermissions.config;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.monospark.actionpermissions.ActionHandler;
+import org.monospark.actionpermissions.ActionSettings;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+final class ConfigGroup {
+
+	private Optional<String> parent;
+	
+	private Map<ActionHandler<?, ?>, ActionSettings> settings;
+
+	private ConfigGroup(Optional<String> parent, Map<ActionHandler<?, ?>, ActionSettings> settings) {
+		this.parent = parent;
+		this.settings = settings;
+	}
+
+	Optional<String> getParent() {
+		return parent;
+	}
+
+	Map<ActionHandler<?, ?>, ActionSettings> getSettings() {
+		return settings;
+	}
+
+	
+	static final class Deserializer implements JsonDeserializer<ConfigGroup> {
+
+		@Override
+		public ConfigGroup deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+				throws JsonParseException {
+			JsonObject object = json.getAsJsonObject();
+			JsonElement parentElement = object.get("parent");
+			Optional<String> parent = parentElement != null ?
+					Optional.of(parentElement.getAsString()) : Optional.empty();
+			Map<ActionHandler<?, ?>, ActionSettings> settings = new HashMap<ActionHandler<?, ?>, ActionSettings>();
+			for (ActionHandler<?,?> handler : ActionHandler.ALL) {
+				JsonElement handlerSettings = object.get(handler.getName());
+				if(handlerSettings != null) {
+					settings.put(handler, context.deserialize(handlerSettings, handler.getSettingsClass()));
+				}
+			}
+			return new ConfigGroup(parent, settings);
+		}
+	}
+}
---
+++ src/main/java/org/monospark/actionpermissions/group/Group.java
@@ -0,0 +1,33 @@
+package org.monospark.actionpermissions.group;
+
+import java.util.Map;
+
+import org.monospark.actionpermissions.ActionHandler;
+import org.monospark.actionpermissions.ActionSettings;
+
+public final class Group {
+	
+	private static final GroupRegistry REGISTRY = new GroupRegistry();
+
+	public static GroupRegistry getRegistry() {
+		return REGISTRY;
+	}
+
+	private String name;
+
+	private Map<ActionHandler<?,?>, ActionSettings> handlers;
+
+	public Group(String name, Map<ActionHandler<?, ?>, ActionSettings> handlers) {
+		this.name = name;
+		this.handlers = handlers;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <S extends ActionSettings> S getActionSettings(ActionHandler<?, S> handler) {
+		return (S) handlers.get(handler);
+	}
+}
