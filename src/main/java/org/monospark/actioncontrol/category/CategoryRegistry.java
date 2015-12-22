3b5c51b73a176c5ba1247e238dfb34b5bb555f5d
Christopher Schnick <c.schnick@monospark.org>
2015-12-22 16:20:16 +0100

    Implement default config generation

--- src/main/java/org/monospark/actioncontrol/category/CategoryRegistry.java
+++ src/main/java/org/monospark/actioncontrol/category/CategoryRegistry.java
@@ -1,7 +1,5 @@
 package org.monospark.actioncontrol.category;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.Set;
@@ -10,8 +8,6 @@ import java.util.stream.Collectors;
 import org.monospark.actioncontrol.config.ConfigParseException;
 import org.monospark.actioncontrol.config.ConfigParser;
 import org.spongepowered.api.entity.living.player.Player;
-
-import com.google.gson.JsonParseException;
 
 public final class CategoryRegistry {
 
@@ -22,23 +18,15 @@ public final class CategoryRegistry {
     CategoryRegistry() {}
 
     public void loadCategories(Path path) throws ConfigParseException {
-        Path categoriesFile = path.resolve("config.json");
-        File file = categoriesFile.toFile();
-        try {
-            if (!file.exists()) {
-                path.toFile().mkdir();
-                file.createNewFile();
-                this.allCategories = Collections.emptySet();
-            } else {
-                this.allCategories = ConfigParser.parseConfig(categoriesFile);
-            }
-        } catch (IOException | JsonParseException e) {
-            this.allCategories = Collections.emptySet();
-            throw new ConfigParseException(e);
-        }
+        allCategories = ConfigParser.loadCategories(path);
     }
 
     public Set<Category> getCategories(Player p) {
+        //If there was an error loading the config file
+        if (allCategories == null) {
+            return Collections.emptySet();
+        }
+
         return allCategories.stream()
                 .filter(c -> /* p.hasPermission(PERMISSION_BASE + c.getName()) */ true)
                 .collect(Collectors.toSet());
--- src/main/java/org/monospark/actioncontrol/config/ConfigParser.java
+++ src/main/java/org/monospark/actioncontrol/config/ConfigParser.java
@@ -11,6 +11,7 @@ import org.monospark.actioncontrol.category.Category;
 import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
 
 public final class ConfigParser {
 
@@ -24,7 +25,23 @@ public final class ConfigParser {
 
     private ConfigParser() {}
 
-    public static Set<Category> parseConfig(Path configFile) throws IOException {
+    public static Set<Category> loadCategories(Path path) throws ConfigParseException {
+        Path categoriesFile = path.resolve("config.json");
+        try {
+            if (!categoriesFile.toFile().exists()) {
+                createDefaultConfig(categoriesFile);
+            }
+            return ConfigParser.parseConfig(categoriesFile);
+        } catch (IOException | JsonParseException e) {
+            throw new ConfigParseException(e);
+        }
+    }
+
+    private static void createDefaultConfig(Path target) throws IOException {
+        Files.copy(ConfigParser.class.getResourceAsStream("config.json"), target);
+    }
+
+    private static Set<Category> parseConfig(Path configFile) throws IOException {
         String contents = new String(Files.readAllBytes(configFile));
         Config config = GSON.fromJson(contents, Config.class);
         return createCategories(config);
